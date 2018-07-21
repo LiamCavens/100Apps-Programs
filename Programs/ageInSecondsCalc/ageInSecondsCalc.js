737646327f308d7fba58873a4dce3c3fe7249b09
Liam Cavens <liamcavens@googlemail.com>
2018-07-21 14:24:46 +0100

    reformatted

--- Programs/ageInSecondsCalc/ageInSecondsCalc.js
+++ Programs/ageInSecondsCalc/ageInSecondsCalc.js
@@ -1,11 +1,9 @@
 function ageInSeconds(years, months, days) {
-    let leapYearSeconds = ((years / 4) * 24 * 60 * 60);
-
-    let yourAgeInSecondsByYear = (years * 365 * 24 * 60 * 60);
+    let yourAgeInSecondsByYear = (years * 365.25 * 24 * 60 * 60);
     let yourAgeinSecondsByMonth = (months * 24 * 60 * 60);
     let yourAgeInSecondsByDay = (days * 60 * 60);
 
-    let yourFullAgeInSeconds = yourAgeInSecondsByYear + yourAgeinSecondsByMonth + yourAgeInSecondsByDay + leapYearSeconds;
+    let yourFullAgeInSeconds = yourAgeInSecondsByYear + yourAgeinSecondsByMonth + yourAgeInSecondsByDay;
 
     return yourFullAgeInSeconds;
 }
