2a3e543567ca29ba10f4a60d7d5c7b8cdff21f09
Liam <liamcavens@googlemail.com>
2018-08-01 12:22:37 +0100

    first run on runcalc working

--- Programs/loveCalculator/loveCalc.js
+++ Programs/loveCalculator/loveCalc.js
@@ -2,8 +2,21 @@ function loveCalc(name1, name2) {
     let loveCount = 0;
     const firstname = name1.toUpperCase();
     const firstnamelength = name1.length;
+    const secondname = name2.toUpperCase();
+    const secondnamelength = name2.length;
 
-    console.log(firstnamelength);
+    for (let count = 0; count < firstnamelength; count++) {
+        let letter1 = firstname.substring(count, count + 1);
+        if (letter1 == 'L') loveCount += 2;
+        if (letter1 == 'O') loveCount += 2;
+        if (letter1 == 'V') loveCount += 2;
+        if (letter1 == 'E') loveCount += 2;
+        if (letter1 == 'Y') loveCount += 3;
+        if (letter1 == 'O') loveCount += 1;
+        if (letter1 == 'U') loveCount += 3;
+    }
+    
+    console.log(loveCount);
     
     
 }
