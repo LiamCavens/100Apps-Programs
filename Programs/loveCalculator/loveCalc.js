b32880b6885444857aceaf90f13a5e0a7500a66c
Liam <liamcavens@googlemail.com>
2018-08-01 12:15:40 +0100

    new file initial commit

---
+++ Programs/loveCalculator/loveCalc.js
@@ -0,0 +1,11 @@
+function loveCalc(name1, name2) {
+    let loveCount = 0;
+    const firstname = name1.toUpperCase();
+    const firstnamelength = name1.length;
+
+    console.log(firstnamelength);
+    
+    
+}
+
+loveCalc("liam", "heather");
