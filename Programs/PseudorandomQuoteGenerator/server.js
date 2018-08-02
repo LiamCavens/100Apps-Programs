69b79ed8763e34e1b5494faf3a86c2362ce15765
Liam <liamcavens@googlemail.com>
2018-08-02 14:33:36 +0100

    added line to express.js

--- Programs/PseudorandomQuoteGenerator/server.js
+++ Programs/PseudorandomQuoteGenerator/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express(); 
 const path = require('path');
 
+express.static
+
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/main.html'));
 });
